cc111043fd53da3b0aded8b3a38d9ff0b27bed47
Dylan Byars <dylan@dylanbyars.com>
2024-07-26 19:57:26 -0400

    make a new initial migration

--- datasource.ts
+++ datasource.ts
@@ -1,3 +1,7 @@
+// NOTE: this was also giving me trouble with the compiler when I put it in `/src`.
+// unlike the migration files, this one doesn't kill the compiler when it's here at the root of the project.
+// And it works for db connections in nest and running the migrations..
+
 import { DataSource } from 'typeorm'
 import { User } from './src/users/user.entity'
 import { Post } from './src/posts/post.entity'
@@ -7,12 +11,12 @@ export const AppDataSource = new DataSource({
   type: 'postgres',
   host: 'localhost',
   port: 5432,
+  // NOTE: using .env or dotenv or AWS secrets or anything is a must instead of hardcoded creds
   username: 'myuser',
   password: 'mypassword',
   database: 'crud_db',
   entities: [User, Post, Comment],
   migrations: ['src/migrations/*.ts'],
-  migrationsTableName: 'migrations',
   logging: true,
   logger: 'advanced-console',
 })
--- src/migrations/1722037484580-CreateTables.ts
+++ src/migrations/1722037484580-CreateTables.ts
@@ -1,14 +1,23 @@
+/*
+
+// NOTE: this is throwing the ts compiler for a loop, saying
+// SyntaxError: Cannot use import statement outside a module
+// idk why this is different from other files here.
+// The migration does work though. This one stands up the tables
+
 import { MigrationInterface, QueryRunner } from 'typeorm'
 
-export class CreateTables1721884017755 implements MigrationInterface {
-  name = 'CreateTables1721884017755'
+export class CreateTables1722037484580 implements MigrationInterface {
+  name = 'CreateTables1722037484580'
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
             CREATE TABLE "comment" (
                 "id" SERIAL NOT NULL,
                 "content" text NOT NULL,
-                "userId" integer,
+                "createdAt" TIMESTAMP NOT NULL DEFAULT now(),
+                "updatedAt" TIMESTAMP NOT NULL DEFAULT now(),
+                "authorId" integer,
                 "postId" integer,
                 CONSTRAINT "PK_0b0e4bbc8415ec426f87f3a88e2" PRIMARY KEY ("id")
             )
@@ -18,7 +27,9 @@ export class CreateTables1721884017755 implements MigrationInterface {
                 "id" SERIAL NOT NULL,
                 "title" character varying NOT NULL,
                 "content" text NOT NULL,
-                "userId" integer,
+                "createdAt" TIMESTAMP NOT NULL DEFAULT now(),
+                "updatedAt" TIMESTAMP NOT NULL DEFAULT now(),
+                "authorId" integer,
                 CONSTRAINT "PK_be5fda3aac270b134ff9c21cdee" PRIMARY KEY ("id")
             )
         `)
@@ -28,13 +39,16 @@ export class CreateTables1721884017755 implements MigrationInterface {
                 "username" character varying NOT NULL,
                 "email" character varying NOT NULL,
                 "password" character varying NOT NULL,
+                "bio" character varying,
+                "createdAt" TIMESTAMP NOT NULL DEFAULT now(),
+                "updatedAt" TIMESTAMP NOT NULL DEFAULT now(),
                 CONSTRAINT "UQ_78a916df40e02a9deb1c4b75edb" UNIQUE ("username"),
                 CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id")
             )
         `)
     await queryRunner.query(`
             ALTER TABLE "comment"
-            ADD CONSTRAINT "FK_c0354a9a009d3bb45a08655ce3b" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION
+            ADD CONSTRAINT "FK_276779da446413a0d79598d4fbd" FOREIGN KEY ("authorId") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION
         `)
     await queryRunner.query(`
             ALTER TABLE "comment"
@@ -42,19 +56,19 @@ export class CreateTables1721884017755 implements MigrationInterface {
         `)
     await queryRunner.query(`
             ALTER TABLE "post"
-            ADD CONSTRAINT "FK_5c1cf55c308037b5aca1038a131" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION
+            ADD CONSTRAINT "FK_c6fb082a3114f35d0cc27c518e0" FOREIGN KEY ("authorId") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION
         `)
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-            ALTER TABLE "post" DROP CONSTRAINT "FK_5c1cf55c308037b5aca1038a131"
+            ALTER TABLE "post" DROP CONSTRAINT "FK_c6fb082a3114f35d0cc27c518e0"
         `)
     await queryRunner.query(`
             ALTER TABLE "comment" DROP CONSTRAINT "FK_94a85bb16d24033a2afdd5df060"
         `)
     await queryRunner.query(`
-            ALTER TABLE "comment" DROP CONSTRAINT "FK_c0354a9a009d3bb45a08655ce3b"
+            ALTER TABLE "comment" DROP CONSTRAINT "FK_276779da446413a0d79598d4fbd"
         `)
     await queryRunner.query(`
             DROP TABLE "user"
@@ -67,3 +81,4 @@ export class CreateTables1721884017755 implements MigrationInterface {
         `)
   }
 }
+*/
